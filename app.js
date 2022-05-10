cdf5bcc3313ce8ba4ce7b153be6c16f417f1f034
Laurentiuq <gheorghe-laurentiu.ghetoiu@s.unibuc.ro>
2022-05-11 00:27:24 +0300

    CRUD

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+
 const express = require('express')
+var formidable = require('formidable');
 const app = express()
 const port = 3000
 app.set('view engine', 'ejs');
@@ -7,11 +9,103 @@ app.set('view engine', 'ejs');
 //   res.send('Hello World!')
 // })
 
+titlu_carti = []
 
 app.use(express.static('static'))
 app.get('/', function(req, res) {
   res.render('index');
 });
+
+// ruta de create
+http://localhost:3000/create
+app.get('/create', function(req, res) {
+  res.render('create');
+});
+
+// adauga un nou titlu de carte
+app.post('/create', (req, res, next) => {
+  const form = formidable({});
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.send(`Ai adaugat o noua carte cu titlul ${fields["titlu"]}`)
+    // res.json(fields);
+    titlu_carti.push(fields["titlu"])
+    console.log(titlu_carti)
+  });
+});
+
+// afiseaza noile titluri de carte adaugate
+//http://localhost:3000/read
+app.get('/read', function(req, res) {
+  res.send(titlu_carti)
+});
+
+// ruta de update
+app.get('/update', function(req, res) {
+  res.render('update');
+});
+
+
+app.post('/update', (req, res, next) => {
+  const form = formidable({});
+
+  form.parse(req, (err, fields) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    // fields["titlu"] -- name din input
+    let ind = titlu_carti.indexOf(fields["titlu"]);
+    // res.json(fields);
+    if(ind!=-1){
+      let titluVechi = titlu_carti[ind]
+      // fields["titluNou"] -- name din input
+      titlu_carti[ind] = fields["titluNou"]
+      res.send(`Ai schimbat tilul cartii din ${titluVechi} in ${fields["titluNou"]}`)
+    }
+    else{
+      res.send(`Cartea nu exista`)
+    }
+    console.log(titlu_carti)
+  });
+});
+
+// ruta de delete
+app.get('/delete', function(req, res) {
+  res.render('delete');
+});
+
+app.post('/delete', (req, res, next) => {
+  const form = formidable({});
+
+  form.parse(req, (err, fields) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    // fields["titlu"] -- name din input
+    let ind = titlu_carti.indexOf(fields["titlu"]);
+    if(ind!=-1){
+      titlu_carti.splice(ind);
+      res.send(`Ai sters cartea cu titlul: ${fields["titlu"]}`)
+    }
+    else{
+      res.send(`Cartea nu exista sau nu a poate fi stearsa`)
+    }
+    console.log(titlu_carti)
+  });
+});
+
+
+app.get('*', function(req, res) {
+  res.render('404');
+});
+
+
 
 // app.get('/images/carteNoua.jpg'), function(req,res){
 //   res.end('carteNoua.jpg');
